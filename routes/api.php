5dcf0b4e1cfa5b7b424e4ae03a8fa2a790114826
Rangga Adithia <rangga.adithia26@gmail.com>
2023-12-11 20:20:43 +0800

    add lab booking api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Hello World!',
+    ], 200);
 });
+
+
+Route::apiResource('bookings', App\Http\Controllers\Api\LabBookingController::class);
